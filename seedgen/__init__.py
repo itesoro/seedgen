2b9d28acda56899b0e1a2c2f8fc9aeffbe7160a9
Ринат Ризванов <rinat@MacBook-Air-Rinat.local>
2021-05-21 19:31:10 +0300

    added asserts and fixed flake8 errors

--- seedgen/__init__.py
+++ seedgen/__init__.py
@@ -5,7 +5,7 @@ import hashlib
 from contextlib import contextmanager
 
 
-ENT = 160
+ENT = 256
 assert ENT % 32 == 0 and 160 <= ENT <= 256
 
 
@@ -18,7 +18,9 @@ def getgetch():
     else:
         yield msvcrt.getch
         return
-    import sys, tty, termios
+    import sys
+    import tty
+    import termios
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     tty.setraw(fd)
@@ -33,7 +35,7 @@ def progress(entropy_bits, need):
     width = 50
     num_filled = int(width * entropy_bits // need)
     bar = '█' * num_filled + ' ' * (width - num_filled)
-    print(f'\r{p:6.1%}|{bar}| {entropy_bits:.0f}/{need} bits', end = '    \r')
+    print(f'\r{p:6.1%}|{bar}| {entropy_bits:.0f}/{need} bits', end='    \r')
     if p < 1:
         return True
     print()
@@ -45,17 +47,22 @@ def to_mnemonic(data, wordlist):
     assert len(wordlist) == 2048
     h = hashlib.sha256(data).hexdigest()
     b = (
-        bin(int.from_bytes(data, byteorder="big"))[2:].zfill(len(data) * 8)
+        bin(int.from_bytes(data, byteorder="big"))[2:].zfill(len(data) * 8)/
         + bin(int(h, 16))[2:].zfill(256)[: len(data) * 8 // 32]
     )
     words = []
     for i in range(len(b) // 11):
-        idx = int(b[i * 11 : (i + 1) * 11], 2)
+        idx = int(b[i * 11:(i + 1) * 11], 2)
         words.append(wordlist[idx])
     return ' '.join(words)
 
 
 def app():
+    wodlist_path = os.path.join(os.path.dirname(__file__), 'wordlist/english.txt')
+    wordlist = [line.strip() for line in open(wodlist_path)]
+    assert to_mnemonic(bytes.fromhex('3f9284bcb5c089863d0c7068a83893944e3b0d48dacf5e60d65b9e27942dfe2b'), wordlist) == 'display neither connect high ancient seek vintage mix hamster dove ceiling chuckle together mammal casino fly fury allow notice detail junk black weather jaguar'  # noqa: E501
+    assert to_mnemonic(bytes.fromhex('335ab07f496eba24ce9671e2c117a5ce0140ee2263aad6f1052c94ac95a37544'), wordlist) == 'crew stereo cabin name two bar demise soda tissue anger truly orchard beef jacket maze inspire street market enroll citizen sing spider steak manage'  # noqa: E501
+    assert to_mnemonic(bytes.fromhex('5737726319c3e98229dc27d261e8241593a8cbad0b2aaa5ebf23eae16c0e2abe'), wordlist) == 'fire romance occur crime direct scissors polar lumber sponsor aunt animal clinic dentist grape reflect grab prevent vote similar still bitter alpha priority scissors'  # noqa: E501
     raw_entropy = bytearray(secrets.token_bytes(32))
     entropy_bits = 0
     bins = {}
@@ -68,6 +75,4 @@ def app():
             entropy_bits += math.log2(p_denom / p_num)
             raw_entropy += c
     data = hashlib.sha512(raw_entropy).digest()[:ENT//8]
-    wodlist_path = os.path.join(os.path.dirname(__file__), 'wordlist/english.txt')
-    wordlist = [line.strip() for line in open(wodlist_path)]
     print(to_mnemonic(data, wordlist))
